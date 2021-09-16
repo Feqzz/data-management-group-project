fef452bf8593bb0264bed524dd5bd7d0ca9b500c
Kent Odde <kentodde89@gmail.com>
2021-09-16 15:46:41 +0200

    Added thomas query

--- query/app.js
+++ query/app.js
@@ -74,45 +74,23 @@ var queries = [
 	"LIMIT 10",
 
 	//Query 3
-	"PREFIX wd: <http://www.wikidata.org/entity/>\n" +
-	"PREFIX wds: <http://www.wikidata.org/entity/statement/>\n" +
-	"PREFIX wdv: <http://www.wikidata.org/value/>\n" +
-	"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
-	"PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
-	"PREFIX p: <http://www.wikidata.org/prop/>\n" +
-	"PREFIX ps: <http://www.wikidata.org/prop/statement/>\n" +
-	"PREFIX pq: <http://www.wikidata.org/prop/qualifier/>\n" +
 	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
 	"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
 	"PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n" +
-	"PREFIX geof: <http://www.opengis.net/def/geosparql/function>\n" +
 	"PREFIX norpark: <http://norpark.ml/>\n" +
 	"PREFIX wikiprop: <https://www.wikidata.org/wiki/Property:>\n" +
 	"PREFIX schema: <http://schema.org/>\n" +
-	"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
 
-	"SELECT ?parking ?electric_chargers ?handicap ?city ?wkt ?wktLabel WHERE\n" +
-	"{\n" +   
-
-		"\tSERVICE <http://query.wikidata.org/sparql>\n" +
-		"\t{\n" +   
-			"\t\t?city wdt:P31 wd:Q755707 .\n" +
-			"\t\t?city rdfs:label ?label .\n" +
-			"\t\t?city rdfs:label \"Kongsberg\"@en\n" +
-			"\t\tFILTER (lang(?label) = \"en\") .\n" +
-		"\t}\n" +  
-
-		"\t?parking wikiprop:P625 ?wkt .\n" +
-		"\t?parking rdf:type norpark:ParkingFacility .\n" +
-		"\t?parking rdfs:label ?wktLabel .\n" +
-		"\t?parking schema:PostalAddress ?a .\n" +
-		"\t?a schema:addressLocality ?city .\n" +
-		"\t?parking norpark:no_of_electric_vehicle_chargers ?electric_chargers .\n" +
-		"\t?parking norpark:no_of_handicap_parking_spaces ?handicap .\n" +
-		"\tFILTER (?handicap > 0) .\n" +
-		"\tFILTER (?electric_chargers > 0) .\n" +
-	"}\n" +  
-	"LIMIT 10"
+	"SELECT ?sub ?lab ?wkt ?wktLabel WHERE {\n" +
+		"?sub rdf:type norpark:ParkingFacility, norpark:ParkingGarage   .\n" +
+		"?sub rdfs:label ?lab .\n" +
+		"?sub wikiprop:P625 ?wkt .\n" +
+		"?sub schema:PostalAddress ?addr .\n" +
+		"?addr schema:addressRegion ?reg .\n" +
+	  
+		"FILTER(?reg = <http://www.wikidata.org/entity/Q5245991> ) .\n" +
+	"} \n" +
+	"LIMIT 25\n"
 ];
 
 //Create yasgui instance
